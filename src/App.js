479972c736844481de6f423327eed299d7d55c64
Kamil <kamilkowicz@gmail.com>
2021-08-07 22:58:50 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Pedigree creator demo</h1>
+      <canvas id="chart"></canvas>
+    </div>
+  );
+}
+
+export default App;
