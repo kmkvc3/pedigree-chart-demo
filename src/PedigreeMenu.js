3bd4bb989553ec1e280cfc4e1f278616ea4b911d
Kamil <kamilkowicz@gmail.com>
2021-08-07 23:04:10 +0200

    integrating modal

--- src/PedigreeMenu.js
+++ src/PedigreeMenu.js
@@ -1,8 +1,15 @@
 import React, { useState } from "react";
 import AddModal from "./Modal";
 
-export default function PedigreeMenu({ x, y, chart, pedigree }) {
+export default function Dots({ x, y, chart, pedigree }) {
   const [modalVisible, setModalVisible] = useState(false)
+
+  function generateParents() {
+    const father = chart.create("male", pedigree.getRawX()-90, pedigree.getRawY()-150);
+    const mother = chart.create("female", pedigree.getRawX()+90, pedigree.getRawY()-150);
+    chart.connect(father, mother, "partnership")
+    chart.connect(mother, pedigree, "sibling")
+  }
 
   return (
     <div
@@ -15,6 +22,7 @@ export default function PedigreeMenu({ x, y, chart, pedigree }) {
       <button onClick={()=>{setModalVisible(true)}}>Add</button>
       <button>Modify</button>
       <button onClick={()=>{chart.delete(pedigree.id)}}>Remove</button>
+      <button onClick={()=>{generateParents()}}>Generate parents</button>
       {modalVisible ? <AddModal pedigree={pedigree} setModalVisible={setModalVisible} chart={chart} /> : null}
     </div>
   );
