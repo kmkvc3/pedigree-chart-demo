20b26afb7e05b2a8e3731ddddb29b55ac5cb9c1d
Kamil <kamilkowicz@gmail.com>
2021-08-07 23:01:14 +0200

    chart init

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import "./App.css";
+import PedigreeChart from "pedigree-chart";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [chart, setChart] = useState(undefined)
+
+  useEffect(() => {
+    const chart = new PedigreeChart();
+    chart.setDiagram("chart");
+    chart.setConfig({
+      width: 1000,
+      height: 500,
+      dragEnabled: true,
+      panEnabled: true,
+    });
+    setChart(chart)
+  }, []);
 
   return (
     <div className="App">
